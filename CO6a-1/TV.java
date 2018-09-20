ed4b3bf124ca477b208c054f7023a717e69f37b2
Lukaa98 <niqabadzeluka@gmail.com>
2018-09-20 19:51:06 -0400

    added TV program

---
+++ CO6a-1/TV.java
@@ -0,0 +1,64 @@
+import java.util.*;
+public class TV
+{
+  private static int count = 0;
+  private String manufacturingDate = "";
+
+  private Date date = new Date();
+
+  int channel = 1;
+  int volumeLevel = 1;
+  boolean on = false;
+
+  public TV()
+  {
+    count++;
+    manufacturingDate = date.toString();
+  }
+
+  public void turnOn()
+  {
+    on = true;
+  }
+
+  public void turnOff()
+  {
+    on = false;
+  }
+
+  public void setChannel(int newChannel)
+  {
+    if(on && newChannel >= 1 && newChannel <= 120)
+      channel = newChannel;
+  }
+
+  public void setVolume(int newVolumeLevel)
+  {
+    if(on && newVolumeLevel >= 1 && newVolumeLevel <= 7)
+      volumeLevel = newVolumeLevel;
+  }
+
+  public void channelUp()
+  {
+    if(on && channel < 120)
+      channel++;
+  }
+
+  public void channelDown()
+  {
+    if(on && channel > 1)
+      channel--;
+  }
+
+  public void volumeUp()
+  {
+    if(on && volumeLevel < 7)
+      volumeLevel++;
+  }
+
+  public void volumeDown()
+  {
+    if(on && volumeLevel > 1)
+      volumeLevel--;
+  }
+}
