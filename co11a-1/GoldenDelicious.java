8d0ae8a8e75c4079cecf4c354f63d2ec92e41e86
Luka Nikabadze <niqabadzeluka@gmail.com>
2018-10-25 16:15:45 -0400

    added GoldenDelicious.java

---
+++ co11a-1/GoldenDelicious.java
@@ -0,0 +1,9 @@
+public class GoldenDelicious extends Apple{
+//data field
+String name;
+//toString() method.
+@Override
+public String toString() {
+return "GoldenDelicious [name=" + super.name + "]";
+}
+}
